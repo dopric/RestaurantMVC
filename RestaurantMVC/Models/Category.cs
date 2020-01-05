dd70ad2db49c8a9a17ad5286592b6d3ad486ad14
Dragan Opric <opric.dragan@gmail.com>
2020-01-05 09:34:07 +0100

    Projektdateien hinzufügen.

---
+++ RestaurantMVC/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantMVC.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength =5)]
+        public string Name { get; set; }
+    }
+}
